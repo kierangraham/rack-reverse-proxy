733f790ae20613db9036d82753cb3ba4a524786f
Kieran Graham <me@kierangraham.com>
2011-08-16 11:47:25 +0100

    fixing something

--- lib/rack/reverse_proxy.rb
+++ lib/rack/reverse_proxy.rb
@@ -44,7 +44,8 @@ module Rack
         when "PUT", "POST"
           req = Net::HTTP.const_get(m.capitalize).new(uri.request_uri, headers)
           req.basic_auth all_opts[:username], all_opts[:password] if all_opts[:username] and all_opts[:password]
-          req.content_length = rackreq.body.size
+          req.content_length = rackreq.body.read.length
+          rackreq.body.rewind
           req.body_stream = rackreq.body
         else
           raise "method not supported: #{m}"
