bd3d2795d7c9e291004049e54a8c10080d5bfb8f
Kieran Graham <me@kierangraham.com>
2011-08-22 17:11:35 +0100

    fixing issue in rack-reverse-proxy where a JSON payload in PUT/POST requests would break

--- lib/rack/reverse_proxy.rb
+++ lib/rack/reverse_proxy.rb
@@ -4,48 +4,43 @@ require 'net/https'
 module Rack
   class ReverseProxy
     def initialize(app = nil, &b)
-      @app = app || lambda {|env| [404, [], []] }
-      @matchers = []
-      @global_options = {:preserve_host => true, :matching => :all, :verify_ssl => true}
+      @app = app || lambda { [404, [], []] }
+      @paths = {}
+      @opts = {:preserve_host => false}
       instance_eval &b if block_given?
     end
 
     def call(env)
       rackreq = Rack::Request.new(env)
-      matcher = get_matcher rackreq.fullpath
+      matcher, url = get_matcher_and_url rackreq.fullpath
       return @app.call(env) if matcher.nil?
 
-      uri = matcher.get_uri(rackreq.fullpath,env)
-      all_opts = @global_options.dup.merge(matcher.options)
+      uri = get_uri(url, matcher, rackreq.fullpath)
       headers = Rack::Utils::HeaderHash.new
       env.each { |key, value|
         if key =~ /HTTP_(.*)/
           headers[$1] = value
         end
       }
-      headers['HOST'] = uri.host if all_opts[:preserve_host]
- 
-      session = Net::HTTP.new(uri.host, uri.port)
-      session.read_timeout=all_opts[:timeout] if all_opts[:timeout]
+      headers['HOST'] = uri.host if @opts[:preserve_host]
 
+      session = Net::HTTP.new(uri.host, uri.port)
       session.use_ssl = (uri.scheme == 'https')
-      if uri.scheme == 'https' && all_opts[:verify_ssl]
-        session.verify_mode = OpenSSL::SSL::VERIFY_PEER
-      else
-        # DO NOT DO THIS IN PRODUCTION !!!
-        session.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      end
+      session.verify_mode = OpenSSL::SSL::VERIFY_NONE
       session.start { |http|
         m = rackreq.request_method
         case m
         when "GET", "HEAD", "DELETE", "OPTIONS", "TRACE"
           req = Net::HTTP.const_get(m.capitalize).new(uri.request_uri, headers)
-          req.basic_auth all_opts[:username], all_opts[:password] if all_opts[:username] and all_opts[:password]
+          req.basic_auth @opts[:username], @opts[:password] if @opts[:username] and @opts[:password]
         when "PUT", "POST"
           req = Net::HTTP.const_get(m.capitalize).new(uri.request_uri, headers)
-          req.basic_auth all_opts[:username], all_opts[:password] if all_opts[:username] and all_opts[:password]
-          req.content_length = rackreq.body.read.length
+          req.basic_auth @opts[:username], @opts[:password] if @opts[:username] and @opts[:password]
+
+          body = rackreq.body.read.to_s
           rackreq.body.rewind
+
+          req.content_length = body.length
           req.body_stream = rackreq.body
         else
           raise "method not supported: #{m}"
@@ -64,17 +59,17 @@ module Rack
 
     private
 
-    def get_matcher path
-      matches = @matchers.select do |matcher|
-        matcher.match?(path)
+    def get_matcher_and_url path
+      matches = @paths.select do |matcher, url|
+        match_path(path, matcher)
       end
 
       if matches.length < 1
         nil
-      elsif matches.length > 1 && @global_options[:matching] != :first
+      elsif matches.length > 1
         raise AmbiguousProxyMatch.new(path, matches)
       else
-        matches.first
+        matches.first.map{|a| a.dup}
       end
     end
 
@@ -88,14 +83,27 @@ module Rack
       response_headers
     end
 
+    def match_path(path, matcher)
+      if matcher.is_a?(Regexp)
+        path.match(matcher)
+      else
+        path.match(/^#{matcher.to_s}/)
+      end
+    end
 
-    def reverse_proxy_options(options)
-      @global_options=options
+    def get_uri(url, matcher, path)
+      if url =~/\$\d/
+        match_path(path, matcher).to_a.each_with_index { |m, i| url.gsub!("$#{i.to_s}", m) }
+        URI(url)
+      else
+        URI.join(url, path)
+      end
     end
 
     def reverse_proxy matcher, url, opts={}
-      raise GenericProxyURI.new(url) if matcher.is_a?(String) && url.is_a?(String) && URI(url).class == URI::Generic
-      @matchers << ReverseProxyMatcher.new(matcher,url,opts)
+      raise GenericProxyURI.new(url) if matcher.is_a?(String) && URI(url).class == URI::Generic
+      @paths.merge!(matcher => url)
+      @opts.merge!(opts)
     end
   end
 
@@ -125,41 +133,8 @@ module Rack
     private
 
     def formatted_matches
-      matches.map {|matcher| matcher.to_s}.join(', ')
+      matches.map {|m| %Q("#{m[0].to_s}" => "#{m[1]}")}.join(', ')
     end
   end
 
-  class ReverseProxyMatcher
-    def initialize(matching,url,options)
-      @matching=matching
-      @url=url
-      @options=options
-      @matching_regexp= matching.kind_of?(Regexp) ? matching : /^#{matching.to_s}/
-    end
-
-    attr_reader :matching,:matching_regexp,:url,:options
-
-    def match?(path)
-      match_path(path) ? true : false
-    end
-
-    def get_uri(path,env)
-      _url=(url.respond_to?(:call) ? url.call(env) : url)
-      if _url =~/\$\d/
-        match_path(path).to_a.each_with_index { |m, i| _url.gsub!("$#{i.to_s}", m) }
-        URI(_url)
-      else
-        URI.join(_url, path)
-      end
-    end
-    def to_s
-      %Q("#{matching.to_s}" => "#{url}")
-    end
-    private
-    def match_path(path)
-      path.match(matching_regexp)
-    end
-
-
-  end
 end
